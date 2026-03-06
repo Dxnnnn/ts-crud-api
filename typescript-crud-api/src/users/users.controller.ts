2e114111c2ea792b783fc0898a9893d71be7ea58
dhexny <dhexny18@gmail.com>
2026-03-06 13:40:58 +0800

    Hands-On Defense Exam (Your Full Name) Midterm

--- typescript-crud-api/src/users/users.controller.ts
+++ typescript-crud-api/src/users/users.controller.ts
@@ -58,7 +58,7 @@ function createSchema(req: Request, res: Response, next: NextFunction): void {
         email: Joi.string().email().required(),
         password: Joi.string().min(6).required(),
         confirmPassword: Joi.string().valid(Joi.ref ('password')).required(),
-        phoneNumber: Joi.string().empty(),
+        phoneNumber: Joi.string().min(10).empty(),
     });
     validateRequest(req, next, schema);
 }
@@ -72,7 +72,7 @@ function updateSchema(req: Request, res: Response, next: NextFunction): void {
         email: Joi.string().email().empty(''),
         password: Joi.string().min(6).empty(''),
         confirmPassword: Joi.string().valid(Joi.ref('password')).empty(''),
-        phoneNumber: Joi.string().empty(),
+        phoneNumber: Joi.string().min(10).empty(),
     }).with('password', 'confirmPassword');
     validateRequest(req, next, schema);
 }
